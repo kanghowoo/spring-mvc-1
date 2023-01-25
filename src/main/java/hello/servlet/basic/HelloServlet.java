5cd1bb9c4aaf7a9e5bb22c0c43882b881bec2da0
KangHo-dev <tiger452003@gmail.com>
2023-01-26 01:29:13 +0900

    Servlet example

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    //서블릿 실행 시, service메소드가 호출된다.
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); //Content-type (헤더 정보)
+        response.setCharacterEncoding("utf-8"); //Content-type (헤더 정보)
+        response.getWriter().write("hello " + username);//http message body 에 들어감.
+
+    }
+}
